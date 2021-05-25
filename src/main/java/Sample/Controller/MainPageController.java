83b2adb01405b3705f5e9b17d31d5623c8e3d92b
naser-kazemi <naserkazemi2002@gmail.com>
2021-05-25 14:21:16 +0430

    update project

--- src/main/java/Sample/Controller/MainPageController.java
+++ src/main/java/Sample/Controller/MainPageController.java
@@ -2,4 +2,15 @@ package Sample.Controller;
 
 public class MainPageController {
 
+    private static MainPageController instance;
+
+
+    private MainPageController() {}
+
+
+    public static MainPageController getInstance() {
+        if (instance == null)
+            instance = new MainPageController();
+        return instance;
+    }
 }
